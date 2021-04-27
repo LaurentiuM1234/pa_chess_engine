f44afad08242b812c23d820e5436c045775753af
Laurentiu Mihalcea <laurentiumihalcea111@gmail.com>
2021-04-27 19:46:42 +0300

    added function documentaion

--- internal/generators/king.h
+++ internal/generators/king.h
@@ -5,6 +5,15 @@
 #include "../../util.h"
 #include "../containers/arraylist.h"
 
+
+/*
+ * Generate all possible king moves for current board's state and side and
+ * put them in a given arraylist object.
+ * @params: moves -> pointer to an arraylist object
+ *          board -> pointer to a board object
+ *          side -> side for which the generation is done
+ * @returns: no return
+ */
 void add_king_moves(arraylist_t *moves, board_t *board, side_t side);
 
 #endif
